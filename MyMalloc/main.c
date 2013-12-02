06d14e3d8ed3e83ac2ea5e858fc2bb865cef13e9
paulinarubio <Paulina@Paulinas-MacBook-Pro.local>
2013-12-02 14:59:12 -0800

    upload
    referencias stackoverflow.com

--- MyMalloc/main.c
+++ MyMalloc/main.c
@@ -8,11 +8,54 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[])
-{
 
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+char memory[100];
+
+
+void initMemory(){
+    int i=0;
+    for(i=0; i<100; i++){
+        
+        memory[i]='?';
+    }
 }
+
+void* malloc_p(int size){
+    
+    int i=0;
+    int crtSize=0;
+    for(i=0; i<100; i++){
+        if (memory[i]=='?'){
+            if(crtSize==size){
+                return &memory[i-size];
+                
+            }
+            crtSize++;
+        } else {
+            crtSize=0;}
+        
+    }
+}
+
+
+
+int main(void){
+    
+    initMemory();
+    char *string =(char*)malloc_p(5 * sizeof(char));
+    strcpy(string,"test\0");
+    printf("string: %s\n", string);
+    
+    int i=0;
+    for(; i < 100; ++i){
+        printf("char: %c\n", memory[i]);
+    }
+    return 0;
+    
+    
+}
+
+
+
+
 
